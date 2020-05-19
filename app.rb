09b0d8f4945e1b82286dc0cd9cc78c7a6fe3c7b4
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-19 10:42:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    
+    erb :index
+  end
 end
